517e103f20013ba909e4cfefd3949c6632103228
leah wanjiku <leagm22@gmail.com>
2022-11-25 15:15:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName ='bob';
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer 
+function setBestCustomer () {
+    window.bestCustomer = 'not bob';
+}
+function  overwriteBestCustomer() {
+    window.bestCustomer ='maybe bob';
+
+}
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = window.leastFavoriteCustomer ='not bob';
+    
+}
+
